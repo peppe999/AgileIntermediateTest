ac42374d873dfba481242788792c86af81a4728e
Giuseppe <peri.cle991@gmail.com>
2021-11-08 16:49:09 +0100

    First commit

---
+++ src/main/java/it/unical/agileintermediatetest/AgileIntermediateTestApplication.java
@@ -0,0 +1,13 @@
+package it.unical.agileintermediatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileIntermediateTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileIntermediateTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unical/agileintermediatetest/AgileIntermediateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.agileintermediatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileIntermediateTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
